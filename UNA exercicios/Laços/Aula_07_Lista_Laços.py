8dc6d753a86f8d5663eb7312e913bc30869aafda
Vitor Gusmão Merlo <vitorgusmaomerlo@gmail.com>
2026-04-09 17:18:20 -0300

    atualização

---
+++ UNA exercicios/Laços/Aula_07_Lista_Laços.py
@@ -0,0 +1,21 @@
+print("01 - Dobro de 5 valores\n")
+valores = []
+for i in range(5):
+    entrada = int(input("Digite um valor: "))
+    valores.append(entrada)
+for v in valores:
+    print(f"dobro do valor {v}: {v * 2}\n")
+
+print("02 - Leitor de números impares de 1 até o número informado\n")
+
+print("03 - Taboada de multiplicar um número até o 10\n")
+
+print("04 - Valor das temperaturas medidas nas 10 primeiras horas do dia 1/01/2024, em Belo Horizonte\n")
+
+print("05 - Homens possuem peso entre 60 e 80 kg\n")
+
+print("06 - Dobro de 5 Valores\n")
+print("07 - Dobro de 5 Valores\n")
+print("08 - Dobro de 5 Valores\n")
+print("09 - Dobro de 5 Valores\n")
+print("10 - Dobro de 5 Valores\n")
